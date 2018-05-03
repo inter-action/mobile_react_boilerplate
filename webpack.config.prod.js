16dc4c042ebd33da43e2a576d2a74ff7ee83a4b8
miao jing <243127395@qq.com>
2018-05-03 14:10:58 +0800

    remove uglifyjs-webpack-plugin, webpack4 support it out of the box

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,5 @@
 const webpack = require('webpack')
 const merge = require('webpack-merge')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const config = require('./webpack.config')
 
 module.exports = merge(config, {
@@ -8,13 +7,6 @@ module.exports = merge(config, {
   plugins: [
     // extractText,
     // Split dependencies into a `vendor` file and provide a manifest
-    new UglifyJSPlugin({
-      parallel: true,
-      uglifyOptions: {
-        warnings: false,
-        ie8: true
-      }
-    }),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify('production')
